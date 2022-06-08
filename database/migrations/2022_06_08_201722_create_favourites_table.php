2b82f002111fc5e2a654e4eb3698f6c90f6acd83
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-08 23:19:18 +0300

    Added a favourites migration

---
+++ database/migrations/2022_06_08_201722_create_favourites_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favourites', function (Blueprint $table) {
+            $table->integer('users_id');
+            $table->integer('products_id');
+            $table->timestamps();
+            $table->foreign('users_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->foreign('products_id')
+                ->references('id')
+                ->on('products')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favourites');
+    }
+};
