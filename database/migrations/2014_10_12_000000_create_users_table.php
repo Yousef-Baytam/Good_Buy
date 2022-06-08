396726d9faa5d80a49581243d256a2f8df475431
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-08 22:56:48 +0300

    Adjusted the User migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,25 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->integer('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('status');
+            $table->integer('users_type_id');
+            $table->integer('cities_id');
             $table->rememberToken();
             $table->timestamps();
+            $table->foreign('users_type_id')
+                ->references('id')
+                ->on('users_type')
+                ->onDelete('cascade');
+            $table->foreign('cities_id')
+                ->references('id')
+                ->on('cities')
+                ->onDelete('cascade');
         });
     }
 
