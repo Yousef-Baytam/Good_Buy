f1d83f9ae92155638391568e6ce4da3f98530d86
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-09 18:54:26 +0300

    Added the initial routes layout

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminProductsController;
+use App\Http\Controllers\AdminUsersController;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\FavouritesController;
+use App\Http\Controllers\ProductsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +25,35 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('register', 'register');
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
+});
+
+Route::group(['prefix' => 'v1'], function () {
+
+    Route::group(['prefix' => 'admin'], function () {
+        Route::group(['middleware' => 'auth:api'], function () {
+            Route::group(['prefix' => 'products'], function () {
+                Route::get('/{id?}', [ProductsController::class, 'getProduct']);
+                Route::post('/add', [AdminProductsController::class, 'addProduct']);
+                Route::patch('/update', [AdminUsersController::class, 'updateProduct']);
+                Route::delete('/delete', [AdminUsersController::class, 'deleteProduct']);
+            });
+            Route::group(['prefix' => 'users'], function () {
+                Route::get('/', [AdminProductsController::class, 'getAllUsers']);
+                Route::post('/suspend/{id}', [AdminProductsController::class, 'suspendUser']);
+                Route::post('/activate/{id}', [AdminProductsController::class, 'activateUser']);
+            });
+        });
+    });
+    Route::group(['prefix' => 'user'], function () {
+        Route::group(['middleware' => 'auth:api'], function () {
+            Route::group(['prefix' => 'products'], function () {
+                Route::get('/{id?}', [ProductsController::class, 'getProduct']);
+                Route::get('/favourite', [FavouritesController::class, 'favouriteProduct']);
+            });
+            Route::group(['prefix' => 'users'], function () {
+                Route::get('/{id}', [UsersController::class, 'getUser']);
+                Route::patch('/update', [UsersController::class, 'updateUser']);
+            });
+        });
+    });
 });
