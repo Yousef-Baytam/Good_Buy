2db8d6a22da0685837ea8275acfc0d2838552aaa
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-09 22:28:12 +0300

    Fixed the namespaces of all the controllers

--- app/Http/Controllers/admin/AdminUsersController.php
+++ app/Http/Controllers/admin/AdminUsersController.php
@@ -1,17 +1,31 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\admin;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AdminUsersController extends Controller
 {
     public function getAllUsers()
     {
+        $users = User::all();
+
+        return response()->json([
+            "status" => "Success",
+            "restos" => $users
+        ], 200);
     }
 
     public function suspendUser($id)
     {
+        $users = User::all();
+
+        return response()->json([
+            "status" => "Success",
+            "restos" => $users
+        ], 200);
     }
 
     public function activateUser($id)
