6c428f963c21307cbf828788685bc18428ffb070
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-09 22:00:05 +0300

    Finilized the initial draft of the products function

--- app/Http/Controllers/products/ProductsController.php
+++ app/Http/Controllers/products/ProductsController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductsController extends Controller
 {
     public function getProduct($id = null)
     {
+        if ($id) {
+            $products = Product::find($id);
+        } else {
+            $products = Product::all();
+        }
+
+        return response()->json([
+            "status" => "Success",
+            "restos" => $products
+        ], 200);
     }
 }
