07f9d513468798db4a4475d02c92d2a18665d39e
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-10 13:38:48 +0300

    Created the categories functions

--- laravel-server/app/Http/Controllers/admin/CategoriesController.php
+++ laravel-server/app/Http/Controllers/admin/CategoriesController.php
@@ -10,17 +10,48 @@ class CategoriesController extends Controller
 {
     public function getCategory($id = null)
     {
+        if ($id) {
+            $cat = Category::find($id);
+        } else {
+            $cat = Category::all();
+        }
+
+        return response()->json([
+            "status" => "Success",
+            "res" => $cat
+        ], 200);
     }
 
     public function addCategory(Request $request)
     {
+        $cat = new Category;
+        $cat->category = $request->category;
+        $cat->save();
+
+        return response()->json([
+            "status" => "Success",
+            "res" => $cat
+        ], 200);
     }
 
     public function updateCategory(Request $request, $id = null)
     {
+        Category::find($id)
+            ->update([
+                'category' => $request->category
+            ]);
+
+        return response()->json([
+            "status" => "Success",
+        ], 200);
     }
 
     public function deleteCategory($id = null)
     {
+        Category::find($id)->delete();
+
+        return response()->json([
+            "status" => "Success",
+        ], 200);
     }
 }
