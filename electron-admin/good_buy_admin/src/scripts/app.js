df416231dccb8741424e6e33c102ce7b4150c964
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-10 22:22:09 +0300

    Debugged and finalised card generation function

--- electron-admin/good_buy_admin/src/scripts/app.js
+++ electron-admin/good_buy_admin/src/scripts/app.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 window.addEventListener('DOMContentLoaded', () => {
-    if (window.location.href.includes('index'))
+    if (window.location.href.includes('index.html'))
         document.querySelector('#login-form').addEventListener('submit', async (e) => {
             e.preventDefault()
             const data = new FormData()
@@ -22,7 +22,6 @@ window.addEventListener('DOMContentLoaded', () => {
         let token
         if (localStorage.getItem('token'))
             token = localStorage.getItem('token')
-        console.log(token)
         let data = new FormData()
         await axios.post('http://127.0.0.1:8000/api/me', data, {
             headers: {
@@ -30,10 +29,9 @@ window.addEventListener('DOMContentLoaded', () => {
             }
         })
             .then((res) => {
-                console.log(res)
-                console.log(window.location.href.includes('admin'))
-                if (res.data.status == 'active' && res.data.users_type_id == 2 && !window.location.href.includes('admin')) {
-                    window.location.href = './admin.html'
+                if (res.data.status == 'active' && res.data.users_type_id == 2) {
+                    if (!window.location.href.includes('admin.html'))
+                        window.location.href = './admin.html'
                 }
 
             })
@@ -46,6 +44,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     if (!window.location.href.includes('index'))
         adminPage()
+
     if (document.querySelector('iframe'))
         if (document.querySelector('iframe').attributes.src.textContent.includes('user'))
             require('./userCards')()
--- electron-admin/good_buy_admin/src/scripts/userCards.js
+++ electron-admin/good_buy_admin/src/scripts/userCards.js
@@ -2,36 +2,61 @@ const axios = require('axios')
 
 module.exports = (arr) => {
     let users
-    axios.get('http://127.0.0.1:8000/api/v1/admin/users')
-
-    const UserCard = `<div class="card-container">
-                        <div class="card">
-                            <div class="details">
-                                <div class="img">
-                                    <img src="../assets/blank-profile.webp" alt="">
-                                </div>
-                                <div class="info">
-                                    <div>Name:</div>
-                                    <div>Email:</div>
-                                    <div>Phone:</div>
-                                </div>
-                            </div>
-                            <div>
-                                <i class="fa-solid fa-ban"></i>
-                                <i class="fa-solid fa-check d-none"></i>
-                            </div>
-                        </div>
-                    </div>`
-
-    const ban = () => {
-        const btn = document.querySelectorAll('i')
-        btn.addEventListener('click', (e) => {
-            console.log('hello')
+    let token
+    if (localStorage.getItem('token'))
+        token = localStorage.getItem('token')
+    axios.get('http://127.0.0.1:8000/api/v1/admin/users/', {
+        headers: {
+            Authorization: `Bearer ${ token }`
+        }
+    })
+        .then((res) => {
+            console.log(res.data.res)
+            users = res.data.res
+            userCardGenerator()
+        }).catch((err) => {
+            console.log(err)
         })
+
+    const userCard = (name, email, phone, id) => {
+        return `<div class="card-container">
+            <div class="card">
+                <div class="details">
+                    <div class="img">
+                        <img src="../assets/blank-profile.webp" alt="">
+                    </div>
+                    <div class="info">
+                        <div id='user_name'>Name: ${ name }</div>
+                        <div id='user_email'>Email: ${ email }</div>
+                        <div id='user_phone'>Phone: ${ phone }</div>
+                    </div>
+                </div>
+                <div>
+                    <i class="fa-solid fa-ban" id='${ id }'></i>
+                    <i class="fa-solid fa-check d-none" id='${ id }'></i>
+                </div>
+            </div>
+        </div>`
     }
 
 
-    const userCardGenerator = () => {
+    const ban = () => {
+        const btn = document.querySelectorAll('i')
+        for (let i of btn) {
+            if (i.classList.contains('fa-ban'))
+                i.addEventListener('click', (e) => {
+                    axios.post('api/v1/admin/users/activate/{id}')
+                })
+        }
 
+    }
+
+    const userCardGenerator = () => {
+        let container = document.querySelector('#iframe').contentDocument.querySelector('.subview')
+        container.innerHTML = ``
+        for (let user of users) {
+            let element = userCard(`${ user.first_name } ${ user.last_name }`, user.email, user.phone, user.id)
+            container.insertAdjacentHTML('beforeend', element)
+        }
     }
 }
