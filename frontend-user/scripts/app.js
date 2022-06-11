9260c7210d65178c7323b4419b0dce52f2b2744e
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-11 20:39:21 +0300

    Added functions to login register and check for a token and redirect

---
+++ frontend-user/scripts/app.js
@@ -0,0 +1,56 @@
+const login = document.querySelector('#login-form')
+
+if (localStorage.getItem('token')) {
+    let token = localStorage.getItem('token')
+    console.log(token)
+    let data = new FormData()
+    axios.post('http://127.0.0.1:8000/api/me', data, {
+        headers: {
+            Authorization: `Bearer ${ token }`
+        }
+    })
+        .then((res) => {
+            if (res.data.id) {
+                window.location.href = './views/products.html'
+            }
+        })
+        .catch((err) => console.log(err))
+}
+
+
+login.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const data = new FormData()
+    data.append('email', document.querySelector('#login-email').value)
+    data.append('password', document.querySelector('#login-password').value)
+
+    await axios.post('http://127.0.0.1:8000/api/login', data)
+        .then((res) => {
+            if (res.data) {
+                localStorage.setItem('token', res.data.authorisation.token)
+                if (res.data.user.status == 'active')
+                    window.location.href = './views/products.html'
+            }
+        })
+        .catch((err) => console.log(err))
+})
+
+const register = document.querySelector('#register-form')
+register.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const data = new FormData()
+    data.append('first_name', document.querySelector('#first_name').value)
+    data.append('last_name', document.querySelector('#last_name').value)
+    data.append('phone', document.querySelector('#phone').value)
+    data.append('email', document.querySelector('#email').value)
+    data.append('password', document.querySelector('#password').value)
+
+    await axios.post('http://127.0.0.1:8000/api/register', data)
+        .then((res) => {
+            if (res.data) {
+                localStorage.setItem('token', res.data.authorisation.token)
+                window.location.href = './products.html'
+            }
+        })
+        .catch((err) => console.log(err))
+})
