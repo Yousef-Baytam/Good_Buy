7d02390d7ad9e0dad82080da2a943e18db7462c7
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-11 22:26:49 +0300

    Adeded function to check the authenticated user info and get the products

--- frontend-user/scripts/app2.js
+++ frontend-user/scripts/app2.js
@@ -1,3 +1,39 @@
+const container = document.querySelector('.container')
+let products
+let token
+let user
+
+if (localStorage.getItem('token')) {
+    token = localStorage.getItem('token')
+    console.log(token)
+    let data = new FormData()
+    axios.post('http://127.0.0.1:8000/api/me', data, {
+        headers: {
+            Authorization: `Bearer ${ token }`
+        }
+    })
+        .then((res) => {
+            if (res.data.id) {
+                user = res.data
+            }
+        })
+        .catch((err) => window.location.href = '../')
+}
+
+const newProducts = () => {
+    axios.get('http://127.0.0.1:8000/api/v1/user/products/', {
+        headers: {
+            Authorization: `Bearer ${ token }`
+        }
+    })
+        .then((res) => {
+            products = res.data.res
+            productCardGenerator()
+        }).catch((err) => {
+            console.log(err)
+        })
+}
+
 const productCard = (name, price, inv, cat, fav, img, id) => {
     return `<div class="product-card-container">
                 <div class="product-card">
@@ -21,4 +57,11 @@ const productCard = (name, price, inv, cat, fav, img, id) => {
                     </div>
                 </div>
             </div>`
+}
+
+const renderCards = (arr) => {
+    container.innerHTML = ``
+    for (let prod of arr) {
+        let card = productCard(prod.product_name, prod.price,)
+    }
 }
